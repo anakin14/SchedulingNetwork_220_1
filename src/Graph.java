e41e3603db1e8afe1529a9849d0d21cab26a3fea
anakin14 <anakin14@users.noreply.github.com>
2017-12-04 00:11:01 -0500

    asdf

--- src/Graph.java
+++ src/Graph.java
@@ -2,45 +2,81 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Graph {
+	int size;
+	LinkedList<Edge> AdjacencyList[];
 	
-	List<List<Edge>> AdjacencyList[];
-	
-	public Graph()
+	public Graph(int size)
 	{
-	AdjacencyList = new LinkedList[9];
-	for(int i = 0; i < 10; i++)
+		this.size = size;
+		AdjacencyList = new LinkedList[size];
+	for(int i = 0; i < 9; i++)
 	{
-		AdjacencyList[i] = new LinkedList<>();
+		AdjacencyList[i] = new LinkedList<Edge>();
 	}
 	}
 	
 	public void add(int i, int j, double weight)
 	{
 		Edge temp = new Edge(j, weight);
+		AdjacencyList[i].add(temp);
 		
 		
 	}
 	
+	public boolean hasEdge(int i, int j)
+	{
+		boolean exit = false;
+		for(int x = 0; !exit; x ++)
+		{
+			try
+			{
+				if(AdjacencyList[i].get(x).ConnectingNode() == j)
+				{
+					return true;
+				}
+			}
+			catch(IndexOutOfBoundsException e)
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	
+	public double print(int i, int j)
+	{
+		for(int x = 0; x < size; x++)
+		{
+			if(AdjacencyList[i].get(x).ConnectingNode == j)
+				return Weight(i,j);
+		}
+		return 0;
+	}
 	
 	
 	public int EarliestArrival()
 	{
-		
+		int i = 0;
+		return i;
 	}
 	 
 	public int LatestTime()
 	{
-		
+		int i = 0;
+		return i;
 	}
 	
 	public int SlackTime()
 	{
-		
+		int i = 0;
+		return i;
 	}
 	
 	public int FloatTime()
 	{
-		
+		int i = 0;
+		return i;
 	}
 	
 	public double Weight(int i, int j)
@@ -50,6 +86,6 @@ public class Graph {
 			if( AdjacencyList[i].get(x).ConnectingNode() == j)
 				return AdjacencyList[i].get(x).Weight();
 		}
-		return -1;
+		return 0;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,64 @@
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Graph graph = new Graph();
+		Graph graph = new Graph(9);
 		
 		
 		graph.add(5, 6, 9);
+		graph.add(6, 7, 8);
+		graph.add(0, 1, 1);
+		graph.add(5, 5, 9);
+		graph.add(2, 3, 9);
+		graph.add(6, 7, 9);
+		graph.add(8, 1, 9);
+		
+		
+		
+		
+		
+		for(int x = 0; x < 10; x++)
+		{
+			for(int y = 0; y < 10; y++)
+				if(graph.hasEdge(x, y))
+				{
+				System.out.println(x + "-> " + y + " weight: " + graph.print(x,y));
+				}
+		}
+		
+		boolean done = false;
+		int entry;
+		System.out.printf("Now that we have the values, enter 1 to calculate\n"
+				+ "the earliest arrival time, 2 to calculate the latest time, 3\n"
+				+ "to calculate slack time and 4 to calculate float time:"
+				+ ".\n");
+		Scanner input = new Scanner(System.in);
+		while(!done){
+			
+		try{
+			System.out.println("Entry: ");
+		entry = input.nextInt();
+		
+		if (entry>=1 && entry<=4)
+		{
+			System.out.println("Complete");
+			done = true;
+		}
+		else
+			System.out.println("Invalid entry; we're only using integers 1-4");
+		}
+		catch(InputMismatchException e){
+			System.out.println("Was not an integer, try again!");
+			input.next();
+		}
 		
 
+		}
 	}
 
 	
