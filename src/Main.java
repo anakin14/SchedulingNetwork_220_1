31e4afdc03449c2d320d1a180999e12aa9bc9bb9
RoddyOfo98 <33762951+RoddyOfo98@users.noreply.github.com>
2017-12-04 02:19:07 -0500

    Add files via upload
    I got the adding expressions to almost work, just not sure if they are actually being added to the graph

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,36 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Graph graph = new Graph(9);
-		
-		
-		graph.add(5, 6, 9);
-		graph.add(6, 7, 8);
-		graph.add(0, 1, 1);
-		graph.add(5, 5, 9);
-		graph.add(2, 3, 9);
-		graph.add(6, 7, 9);
-		graph.add(8, 1, 9);
-		
-		
-		
-		
+		int graphSize = 9;
+		Graph graph = new Graph(graphSize);
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hello, Welcome to our Scheduling Network program.");
+		boolean finishInitializing = false;
+		int initialVertex = 0;
+		int finalVertex = 0;
+		int weight = 0;
+		while(finishInitializing == false){
+			System.out.println("Please enter the initial vertex, terminal vertex and weight of an edge");
+			System.out.println("Or type \"999\" when you are finished with entering edges");
+			initialVertex = Integer.parseInt(input.next());
+			if(initialVertex == 999){
+				finishInitializing = true;
+				return;
+			}
+			finalVertex = Integer.parseInt(input.next());
+			weight = Integer.parseInt(input.next());
+			System.out.println(initialVertex + " " + finalVertex + " " + weight);
+			if(initialVertex > 9 && initialVertex < 0){
+				finishInitializing = true;
+				return;
+			}
+			else{
+				graph.add(initialVertex, finalVertex, weight);
+			}
+			initialVertex = 0;
+			finalVertex = 0;
+			weight = 0;
+		}
 		
 		for(int x = 0; x < 10; x++)
 		{
@@ -37,7 +52,6 @@ public class Main {
 				+ "the earliest arrival time, 2 to calculate the latest time, 3\n"
 				+ "to calculate slack time and 4 to calculate float time:"
 				+ ".\n");
-		Scanner input = new Scanner(System.in);
 		while(!done){
 			
 		try{
